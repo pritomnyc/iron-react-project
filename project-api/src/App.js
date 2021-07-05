1a39b6f9e1a6b1dc638d8ecdbcc8fc3e0317fa53
pritomnyc <mahim.pritom@gmail.com>
2021-07-05 15:34:12 -0400

    pop movies

--- project-api/src/App.js
+++ project-api/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import PopularMovies from "./components/PopularMovies";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" render={(props) => <Navbar />} />
+        <Route exact path="/pop-movies" render={(props) => <PopularMovies />} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+// API stuff
+// featured: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&
+// api_key=https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=892790fdb1ea1d1f1eead753a54cd422&page=1&page=1"
+// images: "https://image.tmdb.org/t/p/w1280"
+// search: "https://api.themoviedb.org/3/search/movie?&
+// api_key=04c35731a5ee918f014970082a0088b1&query="
